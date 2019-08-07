138880dcf4c1f6ce0ec134d91adfd644ee76a729
DanielKovachev <danielkv8@gmail.com>
2019-08-08 01:54:21 +0300

    working gui

---
+++ video_streaming_with_flask_example/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Project: Video Streaming with Flask
+# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
+# Date: 2014/12/21
+# Website: http://www.chioka.in/
+# Description:
+# Modified to support streaming out with webcams, and not just raw JPEGs.
+# Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
+# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
+#
+# Usage:
+# 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
+# 2. Run "python main.py".
+# 3. Navigate the browser to the local webpage.
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+import cv2
+import base64
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    faces= []
+    guns = []
+    for filename in os.listdir('static/images/faces'):
+        faces.append({'id': filename, "photoPath": 'static/images/faces/' + filename})
+    for filename in os.listdir('static/images/guns'):
+        guns.append({'id': filename, "photoPath": 'static/images/guns/' + filename})
+    return render_template('index.html', faces=faces, guns=guns)
+
+@app.route('/objects')
+def objects():
+    faces= []
+    guns = []
+    for filename in os.listdir('static/images/faces'):
+        faces.append({'id': filename, "photoPath": 'static/images/faces/' + filename})
+    for filename in os.listdir('static/images/guns'):
+        guns.append({'id': filename, "photoPath": 'static/images/guns/' + filename})
+    return render_template('object.html', faces=faces, guns=guns)
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
